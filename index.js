af59d8650c68b44c7e3f1e8411c989a677ddbe92
euglazer <eugenestapler@gmail.com>
2016-02-24 11:59:11 -0800

    add window-resize and orbit-controls

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var THREE = require('three')
+var WindowResize = require('three-window-resize')
+var OrbitControls = require('three-orbit-controls')(THREE)
 
 var scene = new THREE.Scene()
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 var renderer = new THREE.WebGLRenderer({alpha: true})
+var windowResize = new WindowResize(renderer, camera)
+var controls = new OrbitControls(camera)
 
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
