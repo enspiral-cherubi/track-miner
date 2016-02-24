22d003c7c9d3d32d1664b3ee725f3b2e3876bc1e
euglazer <eugenestapler@gmail.com>
2016-02-24 13:34:52 -0800

    change color to black

--- src/ring-segment.js
+++ src/ring-segment.js
@@ -2,7 +2,7 @@ var THREE = require('three')
 
 module.exports = function () {
   var geometry = new THREE.CubeGeometry(6, 1, 0.33)
-  var material = new THREE.MeshNormalMaterial({shading: THREE.FlatShading})
+  var material = new THREE.MeshBasicMaterial({color: 0x000000})
   var mesh = new THREE.Mesh(geometry, material)
   return mesh
 }
