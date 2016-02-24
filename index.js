30306853310abcbfe1300ff8f059eb1acc91c1bf
euglazer <eugenestapler@gmail.com>
2016-02-24 11:54:08 -0800

    set background white

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ var THREE = require('three')
 
 var scene = new THREE.Scene()
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-var renderer = new THREE.WebGLRenderer()
+var renderer = new THREE.WebGLRenderer({alpha: true})
 
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var geometry = new THREE.BoxGeometry(1, 1, 1)
+var material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+var cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
 
-camera.position.z = 5;
+camera.position.z = 5
 
 function render() {
 	requestAnimationFrame(render)
