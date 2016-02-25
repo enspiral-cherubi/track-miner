d09ca2eb5d05bbd0ce249df11577ff62d4262b9d
euglazer <eugenestapler@gmail.com>
2016-02-25 02:34:28 -0800

    add flycontrols

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,7 @@
 var THREE = require('three')
 var WindowResize = require('three-window-resize')
 var OrbitControls = require('three-orbit-controls')(THREE)
+require('three-fly-controls')(THREE)
 var PiecewiseRing = require('./piecewise-ring')
 var range = require('lodash.range')
 
@@ -13,19 +14,30 @@ module.exports = {
     this.analyser = analyser
 
     var windowResize = new WindowResize(this.renderer, this.camera)
-    var controls = new OrbitControls(this.camera)
+    // var controls = new OrbitControls(this.camera)
 
     this.renderer.setSize(window.innerWidth, window.innerHeight)
     document.body.appendChild(this.renderer.domElement)
+
+    this.controls = new THREE.FlyControls(this.camera, document.body)
 
     this.camera.position.z = 10000
   },
 
   startAnimation: function () {
     var self = this
-    requestAnimationFrame(function render () {
+    var lastTimeMsec = null
+
+    requestAnimationFrame(function render (nowMsec) {
       requestAnimationFrame(render)
+
+      lastTimeMsec  = lastTimeMsec || nowMsec-1000/60
+      var deltaMsec = Math.min(200, nowMsec - lastTimeMsec)
+      lastTimeMsec  = nowMsec
       self.updateRingWithFrequencyData()
+
+      self.controls.update(deltaMsec/1000)
+
       self.renderer.render(self.scene, self.camera)
     })
   },
@@ -49,7 +61,7 @@ module.exports = {
     this.rings.forEach(function (ring) {
       ring.segments.forEach(function (segment, i) {
         segment.scale.x = frequencyData[i] / 7 + 1
-        segment.scale.z = frequencyData[i] / 20 + 1
+        // segment.scale.z = frequencyData[i] / 20 + 1
       })
     })
   }
