5c90ad18ef7203e5e31fbb4697a7eb0cd9900847
euglazer <eugenestapler@gmail.com>
2016-02-28 17:24:56 -0800

    open / close form btns work

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 var environment = require('./environment')
 var SpectrumAnalyser = require('./spectrum-analyser')
+var $ = require('jquery')
+
+$('#open-url-form-btn').click(function () {
+  $('#url-form-container').show()
+  $(this).hide()
+})
+
+$('#close-url-form-btn').click(function () {
+  $('#url-form-container').hide()
+  $('#open-url-form-btn').show()
+})
 
 var analyser = new SpectrumAnalyser()
-analyser.start(url)
+// analyser.start(url)
 
 environment.init(analyser)
 environment.startAnimation()
