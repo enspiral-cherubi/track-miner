b9e10cd5808fdf247b73b3714bd4c632f93c950b
euglazer <eugenestapler@gmail.com>
2016-02-29 23:58:48 -0800

    created multicolor gradient for piecewise rings

--- src/environment/ring-segment.js
+++ src/environment/ring-segment.js
@@ -2,8 +2,7 @@ var THREE = require('three')
 
 module.exports = function (props) {
   var geometry = new THREE.CubeGeometry(0.5, 3, 5)
-  var material = new THREE.MeshBasicMaterial({color: 0x000000})
-  var mesh = new THREE.Mesh(geometry, material)
+  var mesh = new THREE.Mesh(geometry, props.material)
   mesh.position.x = props.x0 + props.r * Math.cos(props.theta)
   mesh.position.y = props.y0 + props.r * Math.sin(props.theta),
   mesh.position.z = props.z
