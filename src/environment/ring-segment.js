0fdb5b7b4f654887cf8c7448f5be7d8b5aa2c500
Dan Hansen <dan@backerkit.com>
2016-03-04 01:39:22 -0800

    ideas

--- src/environment/ring-segment.js
+++ src/environment/ring-segment.js
@@ -2,10 +2,12 @@ var THREE = require('three')
 
 module.exports = function (props) {
   var geometry = new THREE.CubeGeometry(0.5, 3, 5)
+  //var geometry = new THREE.CylinderGeometry( 1, 1, 1);
   var mesh = new THREE.Mesh(geometry, props.material)
   mesh.position.x = props.x0 + props.r * Math.cos(props.theta)
   mesh.position.y = props.y0 + props.r * Math.sin(props.theta),
   mesh.position.z = props.z
   mesh.rotation.z = props.theta
+  mesh.props = props;
   return mesh
 }
